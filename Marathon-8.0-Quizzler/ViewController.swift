ca6d8991b2adbb7810528d243d819a86b84dcc65
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-08-01 16:10:24 +0500

    Initial Commit

---
+++ Marathon-8.0-Quizzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marathon-8.0-Quizzler
+//
+//  Created by Ислам Пулатов on 8/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
